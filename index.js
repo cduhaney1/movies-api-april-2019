739e354d04c59d0fb0bc7c908db0601d34d9c488
chastity duhaney <chastity_duhaney82@gmail.com>
2019-04-02 14:35:57 -0400

    added the .use to run router on port 4000

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
+// This is standard skeleton code for index.js file to start of basic set up to create a small web app
+
 // Express code that is needed
 const express = require("express");
 
 // Express has to listen on its own port (Note: typically start port at 3000 or higher)
-const PORT = 3000;
+const PORT = 4000;
+
+//Require routers
+const moviesApiRouter = require("./routes/moviesApi");
 
 // I need to create an app with express
 const app = express();
 
 // I need to connect a router in order for everything to connect together
 // Hint: like a switch board
+app.use("/moviesApi", moviesApiRouter);
 
 // I need express to listen on the port
 app.listen(PORT, () => {
